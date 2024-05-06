f935532234eef354e8ae605c2d7bc5b211daae9c
Denise <denisedeedejesus@pm.me>
2024-05-06 01:02:56 -0400

    changed icons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
+
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -42,8 +44,14 @@ const ExpenseItem = (props) => {
         <tr>
             <td className='expense-item'>{props.name}</td>
             <td className='expense-item'>£{props.cost}</td>
-            <td className='expense-item'><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
-            <td className='expense-item'><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+            <td className='expense-item'>
+                {/* <button onClick={event=> decreaseAllocation(props.name)}>-</button> */}
+                <FaMinusCircle className='h3 text-hover text-danger' onClick={event=> decreaseAllocation(props.name)}></FaMinusCircle>
+            </td>
+            <td className='expense-item'>
+                {/* <button onClick={event=> increaseAllocation(props.name)}>+</button> */}
+            <FaPlusCircle className='h3 text-hover text-success' onClick={event=> increaseAllocation(props.name)}></FaPlusCircle>
+            </td>
             <td className='expense-item'><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
