20a66c80782946981c5779452c8666ca5d505ec1
Denise De Jesus <denisedeedejesus@proton.me>
2024-05-13 23:14:44 -0400

    added bg to dropdown as per required

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -18,7 +18,7 @@ const Currency = () => {
     <div className='row'>
         <div className='input-group alert alert-info col'>
                     <label className="input-group-text" htmlFor="selectCurrency">Currency</label>
-                    <select className="custom-select form-select" id="selectCurrency" onChange={handleCurrencyChange}>
+                    <select className="custom-select form-select bg-dark text-white" id="selectCurrency" onChange={handleCurrencyChange}>
                     <option defaultValue value="£" name="GBP">£ Pound</option>
                     <option value="$" name="USD">$ Dollar</option>
                     <option value="€" name="EUR">€ Euro</option>
